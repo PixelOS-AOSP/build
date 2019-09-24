f7856c410cc54b7aece41570a4b3e86a7d8f9816
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 18:45:17 +0200

    releasetools: Move system mount handling to backuptool
    * This allows the ROM to be flashed on any recovery, whether it mounts
      system to /system, /system_root or /mnt/system like Q recovery
    
    * Remove any type of system mount with static paths from updater-script
      to figure out the path dinamically through backuptool
    
    Change-Id: I7b33726305cb6f3eec146d26135f0081002a25dc

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -822,8 +822,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # Stage 3/3: Make changes.
     script.Comment("Stage 3/3")
 
-  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
-
   is_plus = target_info.GetBuildProp("org.pixelexperience.version").endswith(
         "_plus")
   android_version = target_info.GetBuildProp("ro.build.version.release")
@@ -841,10 +839,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.SetPermissionsRecursive("/tmp/install", 0, 0, 0o755, 0o644, None, None)
     script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0o755, 0o755, None, None)
 
+  if target_info.get("system_root_image") == "true":
+    sysmount = "/"
+  else:
+    sysmount = "/system"
+
   if OPTIONS.backuptool:
-    script.Mount("/system")
-    script.RunBackup("backup")
-    script.Unmount("/system")
+    script.RunBackup("backup", sysmount)
 
   # All other partitions as well as the data wipe use 10% of the progress, and
   # the update of the system partition takes the remaining progress.
@@ -881,9 +882,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
-    script.Mount("/system")
-    script.RunBackup("restore")
-    script.Unmount("/system")
+    script.RunBackup("restore", sysmount)
 
   script.WriteRawImage("/boot", "boot.img")
 
